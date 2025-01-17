8229ab0ba823541c09c293919975d0c5a8c923bd
avanimathur <avanimathur24@gmail.com>
2025-01-17 22:01:34 +0530

    add Rewards page, alternate routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import HomePage from './components/home/home';
+import RewardsPage from './pages/RewardsPage';
+import Map from './pages/Map' ;
 
 function App() {
   return (
     <div>
       <HomePage />
+      <Route path="/RewardsPage" element={<RewardsPage />} />
+      <Route path="/Map" element={<Map />} />
     </div>
+
   );
 }
 
